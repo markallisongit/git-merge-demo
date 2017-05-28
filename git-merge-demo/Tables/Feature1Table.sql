c1740631b9d3910cfc571b7851524a12a9beb8aa
Mark Allison <mark.allison@sabin.io>
2017-05-28 20:02:58 +0100

    Made some changes for feature1

---
+++ git-merge-demo/Tables/Feature1Table.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[Feature1Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [SomeText] NVARCHAR(50) NOT NULL
+)
--- git-merge-demo/Tables/MasterTable.sql
+++ git-merge-demo/Tables/MasterTable.sql
@@ -1,5 +1,6 @@
 ﻿CREATE TABLE [dbo].[MasterTable]
 (
 	[Id] INT NOT NULL PRIMARY KEY, 
-    [SomeText] NVARCHAR(50) NOT NULL
+    [SomeText] NVARCHAR(50) NOT NULL, 
+    [Feature1Column] NVARCHAR(50) NULL
 )
